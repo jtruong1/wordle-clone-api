4a5c14dd1371bbebad108e2a1cfe033372fd89d4
James Truong <jtruong123@outlook.com>
2022-03-10 17:55:09 -0800

    Add user sessions

--- config/config.js
+++ config/config.js
@@ -1,9 +1,26 @@
 require('dotenv').config();
 
+const slug = require('slug');
+
 const config = {
+  name: process.env.APP_NAME || 'Wordle Clone',
   env: process.env.APP_ENV || 'local',
   port: process.env.APP_PORT || 3000,
-  secret: process.env.APP_SECRET || 'secret',
+  session: {
+    name: process.env.SESSION_NAME,
+    secret: process.env.SESSION_SECRET || 'secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      sameSite: true,
+      maxAge: process.env.SESSION_LIFETIME || 60 * 60 * 1000,
+    },
+  },
 };
+
+if (!config.session.name) {
+  config.session.name = slug(config.name);
+}
 
 module.exports = config;
--- middleware/index.js
+++ middleware/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const session = require('express-session');
+const config = require('../config/config');
 const state = require('./state');
 
 module.exports = () => {
   const router = express.Router();
 
-  router.use(express.json());
   router.use(cors());
+  router.use(express.json());
+  router.use(session(config.session));
   router.use(state);
 
   return router;
