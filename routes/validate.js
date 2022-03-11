03a5f3150b645510881513d64c521dfdbb748f70
James Truong <jtruong123@outlook.com>
2022-03-10 17:20:52 -0800

    Check if the word is in the list

--- routes/validate.js
+++ routes/validate.js
@@ -6,7 +6,15 @@ const router = express.Router();
 router.post('/', (req, res) => {
   const { state } = req.body;
 
-  res.json(validateWord(state.word, req.app.locals.word));
+  const result = validateWord(state.word, req.app.locals.word);
+
+  if (!result) {
+    return res.json({
+      error: 'Not in word list',
+    });
+  }
+
+  res.json(result);
 });
 
 module.exports = router;
