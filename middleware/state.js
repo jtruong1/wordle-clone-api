e904d90c69c8905c803c27223923341f19dc8b79
James Truong <jtruong123@outlook.com>
2022-03-14 10:47:40 -0700

    Improve daily seed generation

--- middleware/state.js
+++ middleware/state.js
@@ -2,9 +2,13 @@ const md5 = require('md5');
 const config = require('../config/config');
 const { randomWord } = require('../lib/word');
 
+const dailySeed = (salt) => {
+  return md5(new Date(new Date().setUTCHours(0, 0, 0, 0)).getTime() + salt);
+};
+
 module.exports = (req, _res, next) => {
   req.generateState = (seed) => {
-    seed = seed || md5(new Date().toDateString() + config.key);
+    seed = seed || dailySeed(config.key);
 
     req.state = {
       id: seed,
